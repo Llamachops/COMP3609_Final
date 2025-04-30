07a1408efe5976ba3487a079bc3eff295b29af32
Rian Ramdin <rianramdin@gmail.com>
2025-04-30 04:17:52 -0400

    window title

--- GameWindow.java
+++ GameWindow.java
@@ -119,7 +119,7 @@ public class GameWindow extends JFrame implements
 
 	public GameWindow() {
 
-		super("Tiled Bat and Ball Game: Full Screen Exclusive Mode");
+		super("Roland's Quest");
 
 		initFullScreen();
 
